40c69d0395b41a466bb4a7a1236bb4ae66f7b8e9
hyliannprincess <65130269+hyliannprincess@users.noreply.github.com>
2024-04-17 09:14:51 -0400

    Merge file
    this is the file I started work on before the GitHub was made that we will merge

---
+++ CSC316Final/CSC316Final/Game1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CSC316Final
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Model ball, map, bunny, wand, card, die, hat;
+        Matrix camera, projection, ball_world, bunny_world, map_world;
+        int time, lives, score;
+        bool gameStart, gameOver;
+
+        
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+           // ball = Content.Load<Model>();
+            //bunny = Content.Load<Model>();
+            //map = Content.Load<Model>();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ CSC316Final/CSC316Final/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new CSC316Final.Game1();
+game.Run();
